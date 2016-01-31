4e304e2d716ff5f6d165a7b3be00d40b4378c1cc
chapm250 <chapm250@gmail.com>
2016-01-30 20:32:21 -0600

    added more functionality

--- NodePractice/express.js
+++ NodePractice/express.js
@@ -9,8 +9,17 @@ app.use(express.static(__dirname));
 
 
 app.get('/switchVideo', function(req, res){
-    var files = ["videos/test1.webm", "videos/test2.webm", "videos/test3.webm"]
-    res.send(files[Math.floor((Math.random() * 3))]);
+    var files = ["videos/all.webm", "videos/and.webm", "videos/none.webm", "videos/about.webm", "videos/above.webm", "videos/algebra.webm"
+        , "videos/almost.webm", "videos/as.webm", "videos/below.webm", "videos/both.webm", "videos/calculus.webm"
+        , "videos/count.webm", "videos/decrease.webm", "videos/divide.webm", "videos/empty.webm", "videos/enough.webm", "videos/equal.webm"
+        , "videos/fedup.webm", "videos/few.webm", "videos/figure.webm", "videos/full.webm", "videos/geometry.webm", "videos/half.webm"
+        , "videos/heavy.webm", "videos/increase.webm", "videos/large.webm", "videos/light.webm", "videos/limit.webm", "videos/little.webm"
+        , "videos/many.webm", "videos/math.webm", "videos/measure.webm", "videos/more.webm", "videos/most.webm", "videos/much.webm", "videos/none.webm"
+        , "videos/overflow.webm", "videos/percent.webm", "videos/proportion.webm", "videos/small.webm", "videos/statistics.webm", "videos/subtract.webm"
+        , "videos/than.webm", "videos/too.webm", "videos/total.webm", "videos/trigonometry.webm", "videos/very.webm", "videos/weigh.webm", "videos/wide.webm"
+        , "videos/width.webm"];
+
+    res.send(files[Math.floor((Math.random() * files.length))]);
 
 });
 
